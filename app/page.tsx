48deb911452e00de2970173b73f87cbaab17d4e4
yong8048 <water8048@naver.com>
2024-09-21 18:49:05 +0900

    feat: 토큰 확인

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  const token = cookies().get("token");
+  if (!token) {
+    redirect("/login");
+  }
+  return <div></div>;
 }
